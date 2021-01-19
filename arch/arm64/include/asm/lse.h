f35740bcf5470fccb92f9af4935de0c9ba29635f
Sultan Alsawaf <sultan@kerneltoast.com>
2021-01-19 09:51:43 -0800

    arm64: Move LSE atomics out from the alternatives framework
    We know at compile time whether or not our CPU supports LSE atomics,
    so the alternatives framework is only a hindrance, and can reduce
    performance with unnecessary LL/SC register clobbers. This cleans up the
    LSE atomics to depend solely on CONFIG_ARM64_LSE_ATOMICS for determining
    if these instructions are supported. The ARM64_LSE_ATOMIC_INSN macro is
    now gone, and any remaining prefetch instructions that were intended for
    LL/SC via ARM64_LSE_ATOMIC_INSN are now rolled into the LSE usecase,
    since LSE atomics still benefit from prefetching.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Change-Id: Ib1f795f83bb36453febc1b51e5a5365f49a905be
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- arch/arm64/include/asm/lse.h
+++ arch/arm64/include/asm/lse.h
@@ -4,16 +4,9 @@
 
 #if defined(CONFIG_AS_LSE) && defined(CONFIG_ARM64_LSE_ATOMICS)
 
-#include <linux/stringify.h>
-#include <asm/alternative.h>
-
 #ifdef __ASSEMBLER__
 
 .arch_extension	lse
-
-.macro alt_lse, llsc, lse
-	\lse
-.endm
 
 #else	/* __ASSEMBLER__ */
 
@@ -24,34 +17,17 @@ __asm__(".arch_extension	lse");
 #define __LSE_PREAMBLE
 #endif
 
-/* Move the ll/sc atomics out-of-line */
-#define __LL_SC_INLINE		notrace
-#define __LL_SC_PREFIX(x)	__ll_sc_##x
-#define __LL_SC_EXPORT(x)	EXPORT_SYMBOL(__LL_SC_PREFIX(x))
-
-/* Macro for constructing calls to out-of-line ll/sc atomics */
-#define __LL_SC_CALL(op)	""
-#define __LL_SC_CLOBBERS	"x30"
-
-/* In-line patching at runtime */
-#define ARM64_LSE_ATOMIC_INSN(llsc, lse)	lse
+#define ARM64_LSE_ATOMIC_INSN(lse)					\
+	__LSE_PREAMBLE lse
 
 #endif	/* __ASSEMBLER__ */
 #else	/* CONFIG_AS_LSE && CONFIG_ARM64_LSE_ATOMICS */
 
-#ifdef __ASSEMBLER__
-
-.macro alt_lse, llsc, lse
-	\llsc
-.endm
-
-#else	/* __ASSEMBLER__ */
+#ifndef __ASSEMBLER__
 
 #define __LL_SC_INLINE		static inline
 #define __LL_SC_PREFIX(x)	x
 #define __LL_SC_EXPORT(x)
-
-#define ARM64_LSE_ATOMIC_INSN(llsc, lse)	llsc
 
 #endif	/* __ASSEMBLER__ */
 #endif	/* CONFIG_AS_LSE && CONFIG_ARM64_LSE_ATOMICS */
