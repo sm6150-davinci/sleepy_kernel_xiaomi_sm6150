082ad81bed0d5ea942886a16c18608b57ba8e03e
shashank patil <9945shashank@gmail.com>
2023-06-16 07:53:10 +0530

    scripts: update kernel build script * update clang

--- scripts/kernel_build.sh
+++ scripts/kernel_build.sh
@@ -102,7 +102,7 @@ tg_error() {
 
 # clang stuff
 		echo -e "$green << cloning clang >> \n $white"
-		git clone --depth=1 https://gitlab.com/itsshashanksp/aosp-clang-17.0.0.git  "$HOME"/clang
+		git clone --depth=1 https://gitlab.com/itsshashanksp/android_prebuilts_clang_host_linux-x86_clang-r487747c.git  "$HOME"/clang
 
 	export PATH="$HOME/clang/bin:$PATH"
 	export KBUILD_COMPILER_STRING=$("$HOME"/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
