shashank1439/kernel_xiaomi_sm6150

drm: msm: sde: Boost DDR bus when a new frame is ready to be committed
Seems like this is the correct place for this. crtc_commit goes high
in CPU during UiBench invalidade test running top:

CPU usage: 6.0% [crtc_commit:111]

inspired by https://github.com/franciscofranco/beryllium/commit/6f1d917f2dd51130ef26113d025744e95ee11366

Signed-off-by: Shashank Patil <9945shashank@gmail.com>

drivers/gpu/drm/msm/sde/sde_crtc.c
@@ -27,6 +27,7 @@
 #include <drm/drm_flip_work.h>
 #include <linux/clk/qcom.h>
 #include <linux/sde_rsc.h>
+#include <linux/devfreq_boost.h>
 
 #include "sde_kms.h"
 #include "sde_hw_lm.h"
@@ -4318,6 +4319,8 @@ void sde_crtc_commit_kickoff(struct drm_crtc *crtc,
 		return;
 
 	SDE_ATRACE_BEGIN("crtc_commit");
+
+		devfreq_boost_kick(DEVFREQ_CPU_DDR_BW);
 
 	is_error = _sde_crtc_prepare_for_kickoff_rot(dev, crtc);
 

sm6150-davinci/sleepy_kernel_xiaomi_sm6150

drivers/gpu/drm/msm/sde/sde_crtc.c
@@ -27,6 +27,7 @@
 #include <drm/drm_flip_work.h>
 #include <linux/clk/qcom.h>
 #include <linux/sde_rsc.h>
+#include <linux/devfreq_boost.h>
 
 #include "sde_kms.h"
 #include "sde_hw_lm.h"
@@ -4318,6 +4319,8 @@ void sde_crtc_commit_kickoff(struct drm_crtc *crtc,
 		return;
 
 	SDE_ATRACE_BEGIN("crtc_commit");
+
+		devfreq_boost_kick(DEVFREQ_CPU_CPU_LLCC_BW);
 
 	is_error = _sde_crtc_prepare_for_kickoff_rot(dev, crtc);
 